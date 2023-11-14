f7351554a371dc6728bef95dabe27ecfbc669c3d
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2023-11-14 20:19:33 +0530

    #include<stdio.h> int main() { int e,m,p,c,cs,t; scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs); if(e>35 and m>35 and p>35 and c>35 and cs>35) {     printf("PASSED"); } else {     printf("FAILED"); } }

---
+++ Passed_or_Failed.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int e,m,p,c,cs,t;
+scanf("%d%d%d%d%d",&e,&m,&p,&c,&cs);    
+if(e>35 and m>35 and p>35 and c>35 and cs>35) {
+    printf("PASSED");
+}
+else {
+    printf("FAILED");
+}
+}
