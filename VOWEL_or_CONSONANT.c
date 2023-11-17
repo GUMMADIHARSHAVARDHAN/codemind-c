48dbecd81e0bf75cc0d4158f974098d2a9d05e84
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2023-11-17 21:17:48 +0530

    #include<stdio.h> int main() { char ch; scanf("%c",&ch); if(ch=='a') {     printf("VOWEL"); } else if(ch=='e' || ch=='E') { printf("VOWEL"); } else if(ch=='i' ||ch=='I') { printf("VOWEL"); } else if(ch=='o' || ch=='O') { printf("VOWEL"); } else if(ch=='u' || ch=='U') { printf("VOWEL"); } else {     printf("CONSONANT"); } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{ char ch;
+scanf("%c",&ch);    
+if(ch=='a') {
+    printf("VOWEL");
+}    
+else if(ch=='e' || ch=='E') {
+printf("VOWEL");    
+}
+else if(ch=='i' ||ch=='I') {
+printf("VOWEL");    
+}
+else if(ch=='o' || ch=='O') {
+printf("VOWEL");    
+}
+else if(ch=='u' || ch=='U') {
+printf("VOWEL");    
+}
+else {
+    printf("CONSONANT");
+}    
+}
