acb7306f599ce6e514ddb9cee9d4a1a8ad5c495d
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2023-11-17 21:41:23 +0530

    #include<stdio.h> int main() { int cp,sp; scanf("%d%d",&cp,&sp); if(cp == sp) {     printf("No Profit and No Loss"); } else if(cp<sp) {     printf("Profit"); } else {     printf("Loss"); } }

---
+++ Profit_or_Loss.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ int cp,sp;
+scanf("%d%d",&cp,&sp);
+if(cp == sp) {
+    printf("No Profit and No Loss");
+}    
+else if(cp<sp)  
+{
+    printf("Profit");
+}   
+else {
+    printf("Loss");
+}
+}
