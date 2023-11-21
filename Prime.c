b8764e97a767666d45d4f2e82f8cb4fcdaa61b7c
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2023-11-21 20:40:02 +0530

    #include<stdio.h> int main() { int i,n; scanf("%d",&n); int count=0; for(i=1;i<=n;i++) {     if(n%i == 0)     {         count++;     } } if(count == 2) {     printf("Prime"); } else {     printf("Not Prime"); } }

---
+++ Prime.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{ int i,n;
+scanf("%d",&n); 
+int count=0;
+for(i=1;i<=n;i++)
+{ 
+    if(n%i == 0)
+    {
+        count++;
+    }
+}
+if(count == 2)  
+{
+    printf("Prime");
+}    
+else 
+{
+    printf("Not Prime");
+}
+}
