5322ffe7082566e10dba899f8018913917c56f50
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2023-12-20 09:43:04 +0530

    #include<stdio.h> int main() { int n,i,count = 0,c=0; scanf("%d",&n); int a[n]; for(i=0;i<n;i++) { scanf("%d",&a[i]); if(i%2 == 0 and a[i]%2 == 0){     count++; } if(a[i]%2 == 0){     c++; } } if(count == c){     printf("True"); } else {     printf("False"); } }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ int n,i,count = 0,c=0;
+scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+if(i%2 == 0 and a[i]%2 == 0){
+    count++;
+}
+if(a[i]%2 == 0){
+    c++;
+}
+}
+if(count == c){
+    printf("True");
+}
+else {
+    printf("False");
+}
+}
