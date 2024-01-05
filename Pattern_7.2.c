2fa550b091ea8a022cc9b1fabd667b4fcff8acdf
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-01-05 19:48:44 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=n;i>0;i--)     {         for(int j=n;j>0;j--)         {             if(i>=j)             printf("%d ",i);             else             printf(" ");         }         printf(" ");     } }

---
+++ Pattern_7.2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i>0;i--)
+    {
+        for(int j=n;j>0;j--)
+        {
+            if(i>=j)
+            printf("%d ",i);
+            else
+            printf(" ");
+        }
+        printf("
+");
+    }
+}
