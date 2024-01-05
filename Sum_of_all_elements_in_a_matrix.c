7fed549ee702a1b001833f570418cd5239d538cf
HARSHA VARDHAN <147475978+GUMMADIHARSHAVARDHAN@users.noreply.github.com>
2024-01-05 20:28:40 +0530

    #include<stdio.h> int main() {     int r,c,i,j,sum=0;     scanf("%d %d",&r,&c);     int mat[r][c];     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&mat[i][j]);         }     }     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             sum+=mat[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,sum=0;
+    scanf("%d %d",&r,&c);
+    int mat[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            sum+=mat[i][j];
+        }
+    }
+    printf("%d",sum);
+}
